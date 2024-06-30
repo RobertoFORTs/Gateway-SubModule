09c6a234cd61dd4973fb39e52df0e34a53abc00b
RobertoFORTs <bobfortesneto@gmail.com>
2024-06-30 14:12:18 -0400

    feat: add some registering to newsLetter Dtos

---
+++ src/gateway/dtos/create-user.dto.ts
@@ -0,0 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Frequency } from '../enums/frequency';
+
+export class CreateUserDto {
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty({ required: true })
+  email: string;
+
+  @ApiProperty({ enum: Frequency, required: true })
+  frequency: Frequency;
+}
---
+++ src/gateway/enums/frequency.ts
@@ -0,0 +1,6 @@
+export enum Frequency {
+  WEEKLY = 'WEEKLY',
+  BIWEEKLY = 'BIWEEKLY',
+  MONTHLY = 'MONTHLY',
+  SEMESTERLY = 'SEMESTERLY',
+}
