6680c02df441998e64954b41b4d37bc5ef7394ae
RobertoFORTs <bobfortesneto@gmail.com>
2024-06-30 14:33:30 -0400

    fix: fix swagger

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      validateCustomDecorators: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
-    .setTitle('API Gateway')
-    .setDescription('API Gateway para os serviços de Clima e Newsletter')
+    .setTitle('API Climatic Consumer')
+    .setDescription(
+      'Documentação que reúne os endpoints da API Climatic Consumer, que consome a API OpenWeatherMap e retorna informações climáticas de uma cidade específica.',
+    )
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('', app, document);
 
   await app.listen(3000);
 }
+
 bootstrap();
