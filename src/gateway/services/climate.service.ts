472f3136050114c314c1ab957512fdd41a25aa10
RobertoFORTs <bobfortesneto@gmail.com>
2024-07-03 09:53:00 -0400

    fix: better data handling

--- src/gateway/services/climate.service.ts
+++ src/gateway/services/climate.service.ts
@@ -10,7 +10,7 @@ export class ClimateService {
   getAllClimateData(paginateOptions): Observable<any> {
     return this.httpService
       .get(
-        `localhost:3001/climate?page=${paginateOptions.page}&take=${paginateOptions.take}`,
+        `http://localhost:3001/climate?page=${paginateOptions.page}&take=${paginateOptions.take}`,
       )
       .pipe(map((response) => response.data));
   }
@@ -22,8 +22,32 @@ export class ClimateService {
       countryCode: data.countryCode,
       zip: data.zip,
     };
+
     return this.httpService
       .post('http://localhost:3001/climate', requestBody)
-      .pipe(map((response) => response));
+      .pipe(
+        map((response) => {
+          console.log('Climate API Response:', response.data);
+
+          const item = response.data; // Assuming response.data is an object, not an array
+          const transformedResponse = {
+            id: item.id,
+            dateTime: item.dateTime,
+            temperature: item.temperature,
+            humidity: item.humidity,
+            windSpeed: item.windSpeed,
+            climateDescription: item.climateDescription,
+            location: {
+              city: item.location._city,
+              state: item.location._state,
+              country: item.location._country,
+              zip: item.location._zip,
+            },
+            climate: item.climate,
+          };
+
+          return transformedResponse;
+        }),
+      );
   }
 }
--- src/gateway/services/newsletter.service.ts
+++ src/gateway/services/newsletter.service.ts
@@ -12,23 +12,27 @@ export class NewsletterService {
       name: data.name,
       email: data.email,
       frequency: data.frequency,
+      city: data.city,
+      stateCode: data.stateCode,
+      countryCode: data.countryCode,
+      zip: data.zip,
     };
     return this.httpService
-      .post('localhost:5000/user', requestBody)
+      .post('http://localhost:5000/user', requestBody)
       .pipe(map((response) => response.data));
   }
 
   getUser(paginateOptions): Observable<any> {
     return this.httpService
       .get(
-        `localhost:5000/user?page=${paginateOptions.page}&take=${paginateOptions.take}`,
+        `http://localhost:5000/user?page=${paginateOptions.page}&take=${paginateOptions.take}`,
       )
       .pipe(map((response) => response.data));
   }
 
   deleteUser(id: string): Observable<any> {
     return this.httpService
-      .delete(`localhost:5000/user/${id}`)
+      .delete(`http://localhost:5000/user/${id}`)
       .pipe(map((response) => response.data));
   }
 }
